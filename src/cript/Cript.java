f343de76bc8059ae7da97ab6fe4d216e89f0319b
nanoTech Corporate <thiagofigcosta@hotmail.com>
2016-08-27 11:29:03 -0300

    beta2

---
+++ src/cript/Cript.java
@@ -0,0 +1,196 @@
+package cript;
+
+public class Cript {
+    private String decripted="";
+    private String cripted="";
+    private boolean cript=true;
+    
+    private Hash hash;
+    
+    public Cript(Hash hash, String data, boolean cript){
+        this.hash=hash;
+        this.cript=cript;
+        if(cript){
+            this.decripted=data;
+        }else{
+            this.cripted=data;
+        }
+    }
+
+    public Cript() {}
+    
+    public String run(){
+        if(cript)
+            return cript();
+        else
+            return decript();
+    }
+    
+    public String cript(){
+        String out=this.decripted;
+        int steep;
+        boolean firstSteep=false;
+        for(int i=0;i<7;i++){
+            steep=hash.getSteepOrder()[i];
+            if(hash.isAddArraySize()&&i==0&&!firstSteep){
+                steep=3;
+                i--;
+                firstSteep=true;
+            }else if(!(steep==3&&hash.isAddArraySize())){
+            }else{
+                steep=-1;
+            }
+            
+            switch(steep){
+                case 1:
+                    if(hash.isInvertBits())
+                        out=Operations.invert(out);
+                    out=Operations.switchTable(out, true, hash.getSwitchTable());
+                    break;
+                    
+                case 2:
+                    if(hash.isUseAIOBG()){
+                        for(int j=0;j<(int)out.length()/hash.getSize();j++){
+                            if(j%hash.getMultiplesUsingAIOBG()==0){
+                                char temp[]=new char[8];
+                                for(int l=0;l<hash.getSize();l++){
+                                    temp[l]=out.charAt(j*hash.getSize()+l);
+                                }
+                                int t=0;
+                                for(int l=0;l<8;l++){
+                                    if(hash.getIdxAlternativeOfByteGroup()[l]<hash.getSize()){
+                                        out=Operations.replace(out, j*hash.getSize()+t, temp[hash.getIdxAlternativeOfByteGroup()[l]]);
+                                        t++;
+                                    }
+                                    if(t>=hash.getSize())break;
+                                }
+                            }
+                        }
+                    }
+                    break;
+                    
+                case 3:
+                    if(hash.isAddArraySize()){
+                        out=Operations.add(out, hash.getSize(), out.length());
+                    }else{
+                        out=Operations.add(out, hash.getSize(), hash.getAdd());
+                    }
+                    break;
+                    
+                case 4:
+                    out=Operations.pow(out, hash.getSize(), hash.getPow());
+                    out=Operations.mult(out, hash.getSize(), hash.getMult());
+                    break;
+                    
+                case 5:
+                    out=Operations.offset(out, hash.isIsBitOffset1(), hash.getOffset1());
+                    break;
+                    
+                case 6:
+                    out=Operations.offset(out, hash.isIsBitOffset2(), hash.getOffset2());
+                    break;
+                    
+                case 7:
+                    if(hash.getAngularCoefficient()==0)break;
+                    int maxValueOfX=(out.length()-hash.getLinearCoefficient())/hash.getAngularCoefficient();
+                    for(int t=0;t<=maxValueOfX;t++){
+                        int image=hash.getAngularCoefficient()*t+hash.getLinearCoefficient();
+                        String buff1="",buff2="";
+                        buff1=out.substring(0, image-1);//}catch(Exception e){}
+                        buff2=out.substring(image-1);//}catch(Exception e){}
+                        int max=255, min=0;
+                        char random=(char)(min+(int)(Math.random()*((max-min)+1)));
+                        out=buff1+random+buff2;
+                    }
+                    break;
+            }
+        }
+        this.cripted=out;
+        return out;
+    }
+    
+    public String decript(){
+        String out=this.cripted;
+        int steep;
+        boolean lastSteep=false;
+        for(int i=6;i>=0;i--){
+            steep=hash.getSteepOrder()[i];
+            if(hash.isAddArraySize()&&i==1&&!lastSteep){
+                steep=3;
+                lastSteep=true;
+                i++;
+            }else if(!(steep==3&&hash.isAddArraySize())){
+            }else{
+                steep=-1;
+            }
+            
+            switch(steep){
+                case 1:
+                    out=Operations.switchTable(out, false, hash.getSwitchTable());
+                    if(hash.isInvertBits())
+                        out=Operations.invert(out);
+                    break;
+                    
+                case 2:
+                    if(hash.isUseAIOBG()){
+                        for(int j=0;j<(int)out.length()/hash.getSize();j++){
+                            if(j%hash.getMultiplesUsingAIOBG()==0){
+                                char temp[]=new char[8];
+                                for(int l=0;l<hash.getSize();l++){
+                                    temp[l]=out.charAt(j*hash.getSize()+l);
+                                }
+                                int t=0;
+                                for(int l=0;l<8;l++){
+                                    if(hash.getIdxAlternativeOfByteGroup()[l]<hash.getSize()){
+                                        out=Operations.replace(out, j*hash.getSize()+hash.getIdxAlternativeOfByteGroup()[l], temp[t]);
+                                        t++;
+                                    }
+                                    if(t>=hash.getSize())break;
+                                }
+                            }
+                        }
+                    }
+                    break;
+                    
+                case 3:
+                    if(hash.isAddArraySize()){
+                        out=Operations.add(out, hash.getSize(), -out.length());
+                    }else{
+                        out=Operations.add(out, hash.getSize(), -hash.getAdd());
+                    }
+                    break;
+                    
+                case 4:
+                    out=Operations.div(out, hash.getSize(), hash.getMult());
+                    out=Operations.sqrt(out, hash.getSize(), hash.getPow());
+                    break;
+                    
+                case 5:
+                    out=Operations.offset(out, hash.isIsBitOffset1(), -hash.getOffset1());
+                    break;
+                    
+                case 6:
+                    out=Operations.offset(out, hash.isIsBitOffset2(), -hash.getOffset2());
+                    break;
+                    
+                case 7:
+                    if(hash.getAngularCoefficient()==0)break;
+                    int maxValueOfX=(out.length()-hash.getLinearCoefficient())/hash.getAngularCoefficient();
+                    int n=0;
+                    for(int t=0;t<=maxValueOfX;t++){
+                        int image=hash.getAngularCoefficient()*t+hash.getLinearCoefficient();
+                        image-=n;
+                        n++;
+                        String buff1="",buff2="";
+                        buff1=out.substring(0, image-1);
+                        buff2=out.substring(image);
+                        out=buff1+buff2;
+                        maxValueOfX=(out.length()-hash.getLinearCoefficient())/hash.getAngularCoefficient();
+                    }
+                    break;
+            }
+        }
+        this.decripted=out;
+        return out;
+    }
+}
---
+++ src/cript/Operations.java
@@ -0,0 +1,222 @@
+package cript;
+
+import java.math.BigInteger;
+import java.util.Random;
+
+public class Operations {    
+    
+    public static String intToStr(long value, int size){
+        if(size<1||value<0) return "";
+        String bytes="";
+        for(int i=size-1;i>=0;i--){
+            char current=0;
+            for(int j=7;j>=0;j--){
+                if(value>=Math.pow(2, j+i*8)){
+                   current+=Math.pow(2, j);
+                   value-=Math.pow(2, j+i*8);
+                }
+            }
+            bytes+=""+current;
+        }
+        return bytes;
+    }
+    
+    public static long strToInt(String bytes){
+        long out=0;
+        for(int i=0;i<bytes.length();i++)
+            out+=bytes.charAt(i)*Math.pow(2, 8*i);
+        return out;
+    }
+    
+    public static BigInteger strToBInt(String bytes){
+        BigInteger out=BigInteger.ZERO;
+        for(int i=0;i<bytes.length();i++)
+            out=out.add(BigInteger.valueOf(bytes.charAt(i)).multiply(BigInteger.valueOf(2).pow(8*i)));
+        return out;
+    }
+    
+    public static String BintToStr(BigInteger value, int size){
+        if(size<1||value.compareTo(BigInteger.ZERO)<0) return "";
+        String bytes="";
+        for(int i=size-1;i>=0;i--){
+            char current=0;
+            for(int j=7;j>=0;j--){
+                if(value.compareTo(BigInteger.valueOf(2).pow(j+i*8))>=0){
+                   current+=Math.pow(2, j);
+                   value=value.add(BigInteger.valueOf(2).pow(j+i*8).negate());
+                }
+            }
+            bytes+=""+current;
+        }
+        return bytes;
+    }
+    
+    /**
+     * @param bytes sequencia de bytes com valor a ser somado
+     * @param size tamanho dos conjuntos de byte
+     * @param value valor a ser somado
+     * @return string com operacao efetuada
+     */
+    public static String add(String bytes,int size, int value){
+        if(value==0||size<1||size>bytes.length()) return bytes;
+        String out="";
+        int max=bytes.length()/size;
+        if(bytes.length()%size!=0)
+            max++;
+        for(int i=0;i<max;i++){
+            String subStr=""; 
+            for(int l=0;l<size;l++)
+                try{
+                    subStr+=bytes.charAt(i*size+(size-l-1));
+                }catch(Exception e){
+                    String bug=bytes.substring(i*size); 
+                    bug=addLong(bug,1,value);               
+                    return out+bug;
+                } 
+            BigInteger current=strToBInt(subStr);
+            current=current.add(BigInteger.valueOf(value));
+            if(current.compareTo(BigInteger.valueOf(2).pow(8*size))>0)current=current.add(BigInteger.valueOf(2).pow(8*size).negate());
+            if(current.compareTo(BigInteger.ZERO)<0)current=current.add(BigInteger.valueOf(2).pow(8*size));
+ 
+            out+=BintToStr(current,size);
+        }
+        return out;
+    }
+    
+    public static String addLong(String bytes,int size, int value){
+        if(value==0||size<1||size>bytes.length()) return bytes;
+        String out="";
+        int max=bytes.length()/size;
+        if(bytes.length()%size!=0)
+            max++;
+        for(int i=0;i<max;i++){
+            String subStr=""; 
+            for(int l=0;l<size;l++)
+                try{
+                    subStr+=bytes.charAt(i*size+(size-l-1));
+                }catch(Exception e){
+                    String bug=bytes.substring(i*size); 
+                    bug=add(bug,1,value);               
+                    return out+bug;
+                } 
+            long current=strToInt(subStr);
+            current+=value;
+            if(current>Math.pow(2, 8*size))current-=Math.pow(2, 8*size);
+            if(current<0)current+=Math.pow(2, 8*size);  
+            out+=intToStr(current,size);
+        }
+        return out;
+    }
+    
+    public static String pow(String bytes,int size, int value){//TODO: do it
+        return bytes;
+    }
+    
+    public static String sqrt(String bytes,int size, int value){//TODO: do it
+        return bytes;
+    }
+    
+    public static String mult(String bytes,int size, int value){//TODO: do it
+        return bytes;
+    }
+    
+    public static String div(String bytes,int size, int value){//TODO: do it
+        return bytes;
+    }
+    
+    
+    public static String offset(String bytes,boolean bits, int value){
+        if(value>bytes.length())value-=bytes.length();
+        if(value==bytes.length())return bytes;
+        boolean actualBits[]=toBits(bytes);
+        boolean newBits[]=new boolean[bytes.length()*8];
+        if(!bits)value*=8;
+        for(int i=0;i<actualBits.length;i++){
+            int index=i+value;
+            while(index>=actualBits.length){index-=(actualBits.length);}
+            while(index<0){index+=actualBits.length;}
+            newBits[i]=actualBits[index];
+        }
+        return fromBits(newBits);
+    }
+    
+    public static String invert(String bytes){
+        boolean bits[]=toBits(bytes);
+        for(int i=0;i<bits.length;i++){
+            bits[i]=!bits[i];
+        }
+        return fromBits(bits);
+    }
+    
+    public static boolean[] toBits(String bytes){
+        boolean bits[]=new boolean[bytes.length()*8];
+         for(int i=0;i<bytes.length();i++){
+            for(int j=7;j>=0;j--){
+                 bits[7-j+i*8]=false;
+                if(bytes.charAt(i)>=Math.pow(2, j)){
+                    bits[7-j+i*8]=true;
+                    bytes=replace(bytes,i,(char)(bytes.charAt(i)-Math.pow(2, j)));
+                }
+            }
+        }
+        return bits;
+    }
+    
+    public static String fromBits(boolean[] bits){
+        char newBytes[]=new char[bits.length/8];
+        for(int i=0;i<newBytes.length;i++){
+            newBytes[i]=0;
+            for(int j=7;j>=0;j--){
+                if(bits[7-j+i*8])
+                    newBytes[i]+=Math.pow(2, j);
+            }
+        }
+        return String.valueOf(newBytes);
+    }
+    
+    public static String replace(String str, int index, char replace){     
+        if(str==null){
+            return str;
+        }else if(index<0 || index>=str.length()){
+            return str;
+        }
+        char[] chars = str.toCharArray();
+        chars[index] = replace;
+        return String.valueOf(chars);       
+    }
+    
+    public static String switchTable(String bytes, boolean signal, int table){
+        boolean bits[]=toBits(bytes);
+        char sCs[]=new char[bits.length/4];
+        for(int i=0;i<bits.length/4;i++){
+            char semiChar=0;
+            if(bits[i*4+0])semiChar+=8;
+            if(bits[i*4+1])semiChar+=4;
+            if(bits[i*4+2])semiChar+=2;
+            if(bits[i*4+3])semiChar+=1;
+            sCs[i]=Tables.convert(semiChar, signal, table);
+        }
+        char newChar=0;
+        bytes="";
+        for(int i=0;i<sCs.length;i++){
+            if(i%2==0){newChar=(char) (sCs[i]*16);
+            }else{
+                newChar+=sCs[i];
+                bytes+=newChar;
+            }
+            
+        }
+        return bytes;
+    }
+    
+    public static int[] sort(int [] v) {	
+        Random random = new Random();
+        for(int i=0;i<v.length-1;i++){
+            int j=random.nextInt(v.length); 
+            int temp=v[i];
+            v[i]=v[j];
+            v[j]=temp;
+        }
+        return v;
+    }
+}
